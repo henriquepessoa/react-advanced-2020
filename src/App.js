532bfd06f7542405f5484f232c401e6512041732
Henrique Pessoa <henrique.santos@pravaler.com.br>
2021-06-25 11:50:28 -0300

    **

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React from 'react'
+import Final from './tutorial/6-useReducer/final'
+import Setup from './tutorial/6-useReducer/setup'
 function App() {
   return (
     <div className='container'>
-      <h2>Advanced Tutorial</h2>
+      {/* <h2>Use Reducer</h2> */}
+      {/* <Final/> */}
+      <Setup/>
     </div>
   )
 }
